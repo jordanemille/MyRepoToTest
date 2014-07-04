aca1eaf3e2a144f3190dab8d6cb6e00ef19f7892
jordane <JMille@argans.co.uk>
2014-07-04 13:42:34 +0100

    commented code

--- plot_with_func.py
+++ plot_with_func.py
@@ -2,13 +2,13 @@
 
 import math, pylab, scipy, numpy
 
-g = 9.81
-ax = 0
-ay = -9.81
+g = 9.81 # acceleration
+ax = 0 # acceleration from x
+ay = -9.81 # acceleration from y
 v0 = 30.0  # initial velocity
 angle = 30.0 # launch angle (degrees)
-#dt = 0.01  # time step 
-dt= numpy.arange(0,5,0.001)   
+
+dt= numpy.arange(0,5,0.001) # time intervalle
 pylab.xlim(0,80)
 pylab.ylim(0,12)
 
@@ -17,20 +17,19 @@ old_dy = 0.0 # displacement dy (Y-axis) at the initial position = 0
 vx = v0*math.cos(angle*math.pi/180.0) # velocity vx (X-axis)
 vy = v0*math.sin(angle*math.pi/180.0) # velocity vy (Y-axis)
 
-i = 0 # counter
-
+# calcul of the displacement 
 def calcul_displacement(dx,dy,vxi,vyi,dt):
     
     vxi,vyi = calcul_velocity(ax,ay,dt)
     dxi = dx + vxi*dt + (1/2)*ax*dt**2
     dyi = dy + vyi*dt + (1/2)*ay*dt**2
-    return 2*dxi,2*dyi
+    return 2*dxi,2*dyi # 2* is here to make up the intervalle error
 
+# calcul of the velocity
 def calcul_velocity(ax,ay,dt):
     vxi = vx + ax * dt 
     vyi = vy + ay * dt 
     return vxi,vyi
-
 
 
 # MAIN
